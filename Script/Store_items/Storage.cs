f6175f62272b1a519071c025af526742dca6750b
Norbert Muzolf <nmugol000@gmail.com>
2022-11-30 15:56:47 +0100

    New wersion script ading itms to store

--- Script/Store_items/Storage.cs
+++ Script/Store_items/Storage.cs
@@ -1,31 +1,55 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
 
 public class Storage : PanelContainer
 {
-    [Export] private NodePath Grid;
-    [Export] private Texture[] Item_Img;
-    [Export] private int[] Item_Counter;
-    [Export] private NodePath[] Item_Img_Path;//path to texureract
-    [Export] private NodePath[] Item_Counter_Path;//path to label
- 
+    [Export] private Texture[] Item_icons;
+    
     public override void _Ready()
     {
-        GridContainer _Grid = GetNode<GridContainer>(Grid);
-        for(int i=0; i<Item_Counter_Path.Length; i++)
-        {
-            TextureRect _Item = GetNode<TextureRect>(Item_Img_Path[i]);
-            Label _Counter = GetNode<Label>(Item_Counter_Path[i]);
-            
-            _Item.Texture=Item_Img[i];
-            _Counter.Text=Item_Counter[i].ToString();
+        Dictionary<string,int> Items = new Dictionary<string, int>(){
+            {"stone",100},
+            {"irone",200}
+        };
 
-        }
+        Add_Store(Items, this);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void Add_Store(Dictionary<string,int> Items, Node Storeroom)
+    {
+        GridContainer grid = new GridContainer();
+
+        int i=0;
+        foreach(var item in Items)
+        {
+            grid.AddChild(Add_store_items(item.Value,Item_icons[i]));
+            i++;
+        }
+
+        Storeroom.AddChild(grid);
+        grid.Columns=3;
+    }
+
+    private void Remowe_Store(Node Store, Node Storeroom)
+    {
+        Storeroom.RemoveChild(Store);
+        Store.QueueFree();
+    }
+
+    public Node Add_store_items(int how_many, Texture texture)
+    {
+        Label text = new Label();
+        TextureRect icon = new TextureRect();
+        HBoxContainer HBox = new HBoxContainer();
+
+        text.Text = how_many.ToString();
+        icon.Texture = texture;
+
+        HBox.AddChild(icon);
+        HBox.AddChild(text);
+
+        return HBox;
+    }
 }
